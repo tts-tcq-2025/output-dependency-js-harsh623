2e54c2b07454bf5c1959c0cc3cc35aa9d521b48a
harsh623 <73886374+harsh623@users.noreply.github.com>
2025-07-31 09:09:42 +0530

    Update tshirts.mjs

--- tshirts.mjs
+++ tshirts.mjs
@@ -13,4 +13,6 @@ function size(cms) {
 expect(size(37)).equals('S');
 expect(size(40)).equals('M');
 expect(size(43)).equals('L');
+expect(size(38)).equals('M');
+
 console.log('All is well (maybe!)');
